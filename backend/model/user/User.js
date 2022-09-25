5155d46cf7513c1f3cd2d1493b8e68c682785c4d
NTSon <thaisonbk2020@gmail.com>
2022-09-25 14:32:21 +0700

    Create User model

---
+++ backend/model/user/User.js
@@ -0,0 +1,104 @@
+const mongoose = require("mongoose");
+const schemaOptions = require("../../config/model/schemaOptions");
+
+// create Schema
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      required: [true, "First name is required"],
+      type: String,
+    },
+    lastName: {
+      required: [true, "Last name is required"],
+      type: String,
+    },
+    profilePhoto: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2013/07/13/12/07/avatar-159236_960_720.png",
+    },
+    email: {
+      required: [true, "Email is required"],
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Guest", "Blogger"],
+    },
+    isFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isUnFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    accountVerificationToken: String,
+    accountVerificationTokenExpires: Date,
+
+    viewedBy: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+
+    followers: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+
+    following: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+
+    passwordChangeAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
+
+    active: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  schemaOptions
+);
+
+// Compile schema into model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
