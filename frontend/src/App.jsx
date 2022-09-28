efc7e81eeb4d9b816cd27fadd21e356dac61911e
NTSon <thaisonbk2020@gmail.com>
2022-09-28 18:04:30 +0700

    Validate formik resgister page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,21 +11,6 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-
-      <div className="App">
-        <div className="grid grid-cols-10 gap-2">
-          <div className="bg-sky-50 aspect-square"></div>
-          <div className="bg-sky-100 aspect-square"></div>
-          <div className="bg-sky-200 aspect-square"></div>
-          <div className="bg-sky-300 aspect-square"></div>
-          <div className="bg-sky-400 aspect-square"></div>
-          <div className="bg-sky-500 aspect-square"></div>
-          <div className="bg-sky-600 aspect-square"></div>
-          <div className="bg-sky-700 aspect-square"></div>
-          <div className="bg-sky-800 aspect-square"></div>
-          <div className="bg-sky-900 aspect-square"></div>
-        </div>
-      </div>
     </BrowserRouter>
   );
 }
